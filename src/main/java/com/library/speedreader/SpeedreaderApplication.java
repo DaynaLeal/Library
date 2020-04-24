704cc7d24daf8d777cc047a6a2c42ca46e35974b
Dayna Leal <dayna.r.leal@gmail.com>
2020-04-24 10:31:30 -0500

    starting new project

---
+++ src/main/java/com/library/speedreader/SpeedreaderApplication.java
@@ -0,0 +1,13 @@
+package com.library.speedreader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeedreaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpeedreaderApplication.class, args);
+    }
+
+}
