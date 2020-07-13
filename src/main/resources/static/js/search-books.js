e3cc8a4badbdc303e9cd809847eddd26921f1068
Dayna Leal <dayna.r.leal@gmail.com>
2020-07-13 00:03:17 -0500

    search is now dynamic but requires refresh each search

--- src/main/resources/static/js/search-books.js
+++ src/main/resources/static/js/search-books.js
@@ -1,5 +1,51 @@
 $(document).ready(function () {
 
+    $(this).find("#search-btn").click(function () {
+        let fetchInput = 'https://www.googleapis.com/books/v1/volumes?q=';
+        let titleInput = $('#search-title').val();
+        let authorInput = $('#search-author').val();
+        let isbnInput = $('#search-isbn').val();
+        if (isbnInput !== ''){
+            fetchInput += 'isbn=' + isbnInput.replace(/ /g,'');
+        } else {
+            if (titleInput !== ''){
+                fetchInput += 'title=' + titleInput.replace(/ /g,'');
+            }
+            if(authorInput !== ''){
+                fetchInput += 'author=' + authorInput.replace(/ /g,'');
+            }
+        }
+        console.log(fetchInput);
+
+        fetch(fetchInput)
+            .then(function(res) {
+                return res.json();
+            })
+            .then(function(result) {
+                for (let i = 0; i < 10; i++){
+                    let title = result.items[i].volumeInfo.title;
+                    let description = result.items[i].volumeInfo.description;
+                    let image = result.items[i].volumeInfo.imageLinks.thumbnail;
+                    let googleId = result.items[i].id;
+                    let cardDiv = document.getElementById("content");
+                    cardDiv.innerHTML +=
+                        `<div class="col-4">
+                        <div class="card m-2">
+                            <img class="card-img-top" src="` + image + `" alt="Card image cap">
+                            <div class="card-body">
+                                <h5 class="card-title">` + title + `</h5>
+                                <p class="card-text">` + description + `</p>
+                                <input class="btn btn-info" type="submit" value="` + googleId +`" id="search-choice">
+                            </div>
+                        </div>
+                    </div>`;
+                }
+
+            }),
+            function(error) {
+                console.log(error);
+            };
+    })
 
 
 
@@ -9,30 +55,4 @@ $(document).ready(function () {
 
 
 
-fetch("https://www.googleapis.com/books/v1/volumes?q=ibsn:9780747591054")
-    .then(function(res) {
-        return res.json();
-    })
-    .then(function(result) {
-        for (let i = 0; i < 10; i++){
-            let title = result.items[i].volumeInfo.title;
-            let description = result.items[i].volumeInfo.description;
-            let image = result.items[i].volumeInfo.imageLinks.thumbnail;
-            let cardDiv = document.getElementById("content");
-            cardDiv.innerHTML +=
-                `<div class="col-4">
-                        <div class="card m-2">
-                            <img class="card-img-top" src="` + image + `" alt="Card image cap">
-                            <div class="card-body">
-                                <h5 class="card-title">` + title + `</h5>
-                                <p class="card-text">` + description + `</p>
-                                <a href="#" class="btn text-white btn-info">Go somewhere</a>
-                            </div>
-                        </div>
-                    </div>`;
-        }
 
-    }),
-    function(error) {
-        console.log(error);
-    };
